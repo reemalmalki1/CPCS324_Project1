74f2f7a4f82ba6ff5933b70421c1775f097465fd
reema <reema@Reem>
2025-11-06 11:18:12 +0300

    First commit

---
+++ src/main/java/com/mycompany/mstproject1/MSTProject1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mstproject1;
+
+/**
+ *
+ * @author reema
+ */
+public class MSTProject1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
