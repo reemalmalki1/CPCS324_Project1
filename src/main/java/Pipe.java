af459e9f45116b55c5ab24aa871fe2494943700a
Rawan0016 <rmunshi0016@stu.kau.edu.sa>
2025-11-12 15:53:57 +0300

    Update Pipe.java

--- src/main/java/Pipe.java
+++ src/main/java/Pipe.java
@@ -4,19 +4,16 @@ public class Pipe extends Edge {
     private String material;
     private double maxPressure;
     private boolean isLeaking;
-    private String installationDate;
-    private boolean isActive;
     
     public Pipe(Junction source, Junction destination, int weight) {
         super(source, destination, weight);
     }
 
-    public Pipe(String material, double maxPressure, boolean isLeaking, boolean isActive, Junction source, Junction destination, int weight) {
+    public Pipe(String material, double maxPressure, boolean isLeaking, Junction source, Junction destination, int weight) {
         super(source, destination, weight);
         this.material = material;
         this.maxPressure = maxPressure;
         this.isLeaking = isLeaking;
-        this.isActive = isActive;
     }
 
     public String getMaterial() {
@@ -43,28 +40,11 @@ public class Pipe extends Edge {
         this.isLeaking = isLeaking;
     }
 
-    public String getInstallationDate() {
-        return installationDate;
-    }
-
-    public void setInstallationDate(String installationDate) {
-        this.installationDate = installationDate;
-    }
-
-    public boolean isIsActive() {
-        return isActive;
-    }
-
-    public void setIsActive(boolean isActive) {
-        this.isActive = isActive;
-    }
-
     @Override
     public String toString() {
         return "Pipe{" + "source=" + source + ", destination=" + destination 
                 +", weight=" + weight + "material=" + material + ", maxPressure="
-                + maxPressure + ", isLeaking=" + isLeaking + ", installationDate=" 
-                + installationDate + ", isActive=" + isActive + '}';
+                + maxPressure + ", isLeaking=" + isLeaking + '}';
     }
     
 }
