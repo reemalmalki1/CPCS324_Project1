707bf034305b504890519b0c132933624c98d3af
Reem Almalki <ralmalki0605@stu.kau.edu.sa>
2025-11-12 21:24:26 +0300

    Update Vertex.java

--- src/main/java/Vertex.java
+++ src/main/java/Vertex.java
@@ -1,15 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
 /**
  *
  * @author reem
  */
 public class Vertex {
     String label; // Vertex name like J1, J2
-
     public Vertex(String label) {
         this.label = label;
     }
