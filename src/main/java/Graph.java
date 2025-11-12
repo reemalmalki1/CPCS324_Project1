5900f152be161417028a13b85bc241465e9f7bef
Reem Almalki <ralmalki0605@stu.kau.edu.sa>
2025-11-12 21:22:57 +0300

    Update Graph.java

--- src/main/java/Graph.java
+++ src/main/java/Graph.java
@@ -1,35 +1,25 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
 /**
  *
  * @author reem
  */
 import java.io.File;
 import java.util.*;
-
 public class Graph {
     List<Vertex> vertices; // List of all vertices
     List<Edge> edges;      // List of all edges
-
     public Graph() {
         vertices = new ArrayList<>();
         edges = new ArrayList<>();
     }
-
     // Read graph data from external file
     public void readGraphFromFile(String filename) {
         try {
             File file = new File(filename);
             Scanner sc = new Scanner(file);
-
             while (sc.hasNext()) {
                 String v1Label = sc.next();
                 String v2Label = sc.next();
                 int weight = sc.nextInt();
-
                 addEdge(v1Label, v2Label, weight);
             }
             sc.close();
@@ -37,7 +27,6 @@ public class Graph {
             System.out.println("Error reading file: " + e.getMessage());
         }
     }
-
     // Add edge between two vertex labels
     private void addEdge(String label1, String label2, int weight) {
         Vertex v1 = getOrCreateVertex(label1);
@@ -45,21 +34,18 @@ public class Graph {
         Edge edge = new Edge(v1, v2, weight);
         edges.add(edge);
     }
-    
     // Add edge 
     public void addEdge(Edge edge) {
         if(!edges.contains(edge)){
             edges.add(edge);
         }
     }
-    
      // Add Vertex 
     public void addVertex(Vertex v) {
         if(!vertices.contains(v)){
             vertices.add(v);
         }
     }
-
     // Find or create a vertex
     private Vertex getOrCreateVertex(String label) {
         for (Vertex v : vertices) {
@@ -70,7 +56,6 @@ public class Graph {
         vertices.add(newV);
         return newV;
     }
-
     // Print all edges
     public void printGraph() {
         for (Edge e : edges) {
